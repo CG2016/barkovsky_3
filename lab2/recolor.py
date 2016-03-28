69f16b1104864f1f5cbc80c7a2ee7aea4f8e18fe
Anton Barkovsky <anton@swarmer.me>
2016-03-28 17:41:15 +0300

    Convert using lab

--- lab2/recolor.py
+++ lab2/recolor.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 import argparse
 import tkinter as tk
+import math
 
+import numpy
 import PIL.ImageTk
 import PIL.Image
+from colormath.color_objects import sRGBColor, LabColor
+from colormath.color_conversions import convert_color
+from colormath.color_diff import delta_e_cie2000
 
 
 IMAGE_SIZE = (600, 400)
 
 
-def get_scaled_image(image_path):
-    image = PIL.Image.open(image_path)
-    image.thumbnail(IMAGE_SIZE)
-    return image
+def cap_number(number, min_, max_):
+    if number < min_:
+        return min_
+    elif number > max_:
+        return max_
+    else:
+        return number
 
 
 class RecolorWindow:
@@ -23,6 +31,9 @@ class RecolorWindow:
 
         self._original_tk_image = None
         self._result_tk_image = None
+
+        self._from_lab = None
+        self._to_lab = None
 
         self.label_original = tk.Label(self.root)
         self.label_original.grid(row=0, column=0, columnspan=3)
@@ -68,11 +79,11 @@ class RecolorWindow:
         )
         self.to_b_scale.grid(row=1, column=5, sticky='nsew')
 
-        self.range = tk.Scale(
+        self.range_scale = tk.Scale(
             self.root, from_=0, to=255,
             orient=tk.HORIZONTAL, label='Range'
         )
-        self.range.grid(row=2, column=0, sticky='nsew')
+        self.range_scale.grid(row=2, column=0, sticky='nsew')
 
         self.button = tk.Button(self.root, text="Recolor", command=self.recolor)
         self.button.grid(row=3, column=0, sticky='nsew')
@@ -91,8 +102,90 @@ class RecolorWindow:
         self._result_tk_image = PIL.ImageTk.PhotoImage(scaled_image)
         self.label_result.config(image=self._result_tk_image)
 
+    @property
+    def from_color(self):
+        red = self.from_r_scale.get()
+        green = self.from_g_scale.get()
+        blue = self.from_b_scale.get()
+        return (red, green, blue)
+
+    @property
+    def to_color(self):
+        red = self.to_r_scale.get()
+        green = self.to_g_scale.get()
+        blue = self.to_b_scale.get()
+        return (red, green, blue)
+
+    @property
+    def range(self):
+        return self.range_scale.get()
+
     def recolor(self):
-        pass
+        self._from_lab = convert_color(sRGBColor(*self.from_color), LabColor)
+        self._to_lab = convert_color(sRGBColor(*self.to_color), LabColor)
+        range_ = self.range
+        from_r, from_g, from_b = self.from_color
+
+        width, height = self.image.width, self.image.height
+        source_pixels = numpy.asarray(self.image)
+
+        result_image = PIL.Image.new('RGB', (width, height), "black")
+        target_pixels = result_image.load()
+
+        pixel_count = 0
+        for i in range(width):
+            for j in range(height):
+                r, g, b = source_pixels[i, j]
+
+                distance = math.sqrt(
+                    (r - from_r) ** 2 +
+                    (g - from_g) ** 2 +
+                    (b - from_b) ** 2
+                )
+
+                if distance > range_:
+                    continue
+
+                source_lab = convert_color(sRGBColor(r, g, b), LabColor)
+                l_diff = source_lab.lab_l - self._from_lab.lab_l
+                a_diff = source_lab.lab_a - self._from_lab.lab_a
+                b_diff = source_lab.lab_b - self._from_lab.lab_b
+
+                l_new = cap_number(self._to_lab.lab_l + l_diff, 0, 100)
+                a_new = cap_number(self._to_lab.lab_a + a_diff, -128, 128)
+                b_new = cap_number(self._to_lab.lab_b + b_diff, -128, 128)
+                srgb = convert_color(LabColor(l_new, a_new, b_new), sRGBColor)
+                target_pixels[i, j] = (
+                    int(srgb.rgb_r), int(srgb.rgb_g), int(srgb.rgb_b)
+                )
+
+                pixel_count += 1
+                if pixel_count % 200000 == 0:
+                    print(pixel_count)
+                    return
+
+        self.set_result_image(result_image)
+
+    def _calc_color(self, source_color):
+        r, g, b = source_color
+        delta_e = delta_e_cie2000(
+            self._from_lab,
+            convert_color(sRGBColor(r, g, b), LabColor),
+        )
+
+        if delta_e > self.range:
+            return tuple(source_color)
+
+        source_lab = convert_color(sRGBColor(r, g, b), LabColor)
+        l_diff = source_lab.lab_l - self._from_lab.lab_l
+        a_diff = source_lab.lab_a - self._from_lab.lab_a
+        b_diff = source_lab.lab_b - self._from_lab.lab_b
+
+        l_new = cap_number(self._to_lab.lab_l + l_diff, 0, 100)
+        a_new = cap_number(self._to_lab.lab_a + a_diff, -128, 128)
+        b_new = cap_number(self._to_lab.lab_b + b_diff, -128, 128)
+        srgb = convert_color(LabColor(l_new, a_new, b_new), sRGBColor)
+        return (srgb.rgb_r, srgb.rgb_g, srgb.rgb.b)
 
 
 def main():
